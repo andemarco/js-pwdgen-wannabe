d3e1d5ad0a66a7b514f3afb7b57b4c62f59a51ff
Andrea De Marco <andreademarco91@gmail.com>
2019-12-17 17:10:17 +0100

    PWD Generator OK + style

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // Chiedi all’utente il suo nome
-var name = prompt('Inserisci qui il tuo Nome')
+var name = prompt('Inserisci qui il tuo Nome');
 // chiedi il suo cognome,
-var secondName = prompt('Inserisci qui il tuo Cognome (senza spazi)')
+var secondName = prompt('Inserisci qui il tuo Cognome (senza spazi)');
 // chiedi il suo colore preferito.
-var prefColor = prompt('Indica qui il tuo colore preferito')
+var prefColor = prompt('Indica qui il tuo colore preferito');
 
-document.getElementById('pwd-gen').innerHTML = name + secondName + prefColor + '19'
+document.getElementById('pwd-gen').innerHTML = name + secondName + prefColor + '19';
